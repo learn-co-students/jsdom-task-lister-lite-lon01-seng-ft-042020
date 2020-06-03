ed0c7433d1fb2710e11922cd0de39ac6fb994a7a
zbadiru <zbadiru@live.co.uk>
2020-06-03 16:45:29 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const newTaskUl = document.getElementById("tasks")
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+const deleteButton = document.querySelector("#delete")
+
+deleteButton.addEventListener("click", function (event) {
+  const ul = document.querySelector("#tasks")
+  ul.removeChild(ul.querySelector('li:nth-child(1)'))
+})
+
+// did not finish all the stretch deliverables, only managed the delete and drop down.
