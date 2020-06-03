cabc7974466e01929c2e4e4b97090aef23c76ac9
gojoaromokudu <gojoaromokudu@gmail.com>
2020-06-03 16:48:15 +0100

    finished the basic tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // defined all needed variables
+  const taskForForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector('#tasks');
+
+  taskForForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // debugger
+    // logs event to console
+    console.log(event);
+
+    // find the text from the form and set as a variable
+    const taskText = event.target[0].value;
+    // create the the li tag from user input
+    const taskDOMNodeLi = document.createElement("li");
+    // set the text of the li to the variable of the user input
+    taskDOMNodeLi.innerText = taskText;
+    // add the li to the list
+    taskList.appendChild(taskDOMNodeLi);
+
+    const button = document.createElement("button");
+    button.innerHTML = "x";
+
+    // 2. Append somewhere
+    // const body = document.getElementsByTagName("body")[0];
+    taskDOMNodeLi.appendChild(button);
+
+    // 3. Add event handler
+    button.addEventListener("click", function () {
+      taskDOMNodeLi.remove();
+      alert("You deleted this task");
+    });
+
+  })
 });
