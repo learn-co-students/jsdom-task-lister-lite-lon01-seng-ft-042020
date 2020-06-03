343ba86a97684915c42ddad9000cd364fbc94f39
github username <github email address>
2020-06-03 18:14:25 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -13,10 +13,42 @@ document.addEventListener("DOMContentLoaded", () => {
     //Our code goes here
     console.log(event)
     //Stops the page from reloading immediately, so we can play around with the code
-    event.preventDefault() 
-    let textWritten = event.target[0].value
+    event.preventDefault() ;
+    let textWritten = event.target[0].value;
     //debugger;
-  } )
+    //Find the ul that we are gona add info to 
+    const findUl = document.getElementById('tasks');
+    
+    //Create the list element
+    const createLi = document.createElement('li');
+    
+    //Add the given text from the user as the value of the <li>
+    createLi.innerHTML = textWritten;
+    
+    //Add list element to the ul we found
+    findUl.appendChild(createLi);
+
+    //Create the button for each list element
+    const createButton = document.createElement('button');
+
+    //Make the button an x
+    createButton.innerHTML = "x";
+
+    //Add a class attribute to to the button tag
+    //The value is the text given by the user
+    createButton.setAttribute("class", `${textWritten}`);
+
+    //Add the button to the <li> element
+    createLi.appendChild(createButton);
+
+    //Create an event listener on the button 
+    //Which removes the <li> element when it is clicked
+    //Q: Why dont we need a preventDefault in this event
+    createButton.addEventListener("click", function (event) {
+      event.target.parentNode.remove();
+    });
+
+  })
 
 });
 
