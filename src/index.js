de815c9f0fb6df8f1612fdeca89dd6e7b8955214
CharliePayne1 <charliepayne1101@gmail.com>
2020-06-03 16:55:21 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+// type a task into the field
+let taskform = document.querySelector('#create-task-form');
+
+let listItems = document.getElementById('tasks');
+//  submit the task 
+taskform.addEventListener("submit", addTasks);
+
+
+function addTasks(e) {
+  let text = document.createElement('li');
+
+  e.preventDefault()
+  const target = e.target[0].value;
+  const priority = e.target[1].value;
+
+  if (priority === 'red') {text.style.color = 'red'}
+  if (priority === 'yellow') {text.style.color = 'yellow'}
+  if (priority === 'green') {text.style.color = 'green'}
+  
+  text.addEventListener("click", function (e) {
+    e.target.remove();
+  });
+  text.textContent = target;
+
+  listItems.append(text);
+  document.querySelector("#create-task-form").reset()
+
+}
+
