f826679cf08a233845797a607687944722466ce5
github username <github email address>
2020-06-10 01:28:37 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   //Find the whole form
   const taskForm = document.querySelector('#create-task-form')
 
-  //Add event listener method on the element we found
+  //Add event listener method on the form element we found
   //This method takes 2 arguments 
   //1st: The event - the action that happens to the element
   //2nd: A function, that takes the argument of the event that's happened
   //& tells us what to do with the info we have gotten 
   taskForm.addEventListener("submit", function(event) {
-    //Our code goes here
-    console.log(event)
+  
     //Stops the page from reloading immediately, so we can play around with the code
     event.preventDefault() ;
     let textWritten = event.target[0].value;
     //debugger;
-    //Find the ul that we are gona add info to 
+    //Find the ul that we will add info to 
     const findUl = document.getElementById('tasks');
     
     //Create the list element
